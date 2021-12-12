eaec54d3a2b1f6dc39422940cee42d3c72c93325
zahidkhan262 <zahidkhan007860@gmail.com>
2021-12-12 22:00:54 +0530

    oposite pyramid

--- pattern.js
+++ pattern.js
@@ -3,10 +3,26 @@
 var userInput = 5;
 
 let star = "";
-for (let i = 1; i <= userInput; i++) {
-    for (let j = 0; j < i; j++) {
+// for (let i = 1; i <= userInput; i++) {
+//     for (let j = 0; j < i; j++) {
+//         star += "*";
+//     }
+//     star += "\n";
+// }
+// console.log(star);
+// op=>
+// *
+// **
+// ***
+// ****
+// *****
+
+for (let i = 0; i < userInput; i++) {
+    for (let j = 0; j < userInput - i; j++) {
         star += "*";
     }
     star += "\n";
 }
 console.log(star);
+
+
