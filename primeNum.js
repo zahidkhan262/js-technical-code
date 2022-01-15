d90a32814f53a43546dbd5d16311a453eccd859b
zahidkhan262 <zahidkhan007860@gmail.com>
2022-01-15 22:20:25 +0530

    primeNumber

--- primeNum.js
+++ primeNum.js
@@ -1,18 +1,46 @@
 // what is prime number?
 // which no diveded by 1 or iteself also called prime number
 
-let user = 7;
-let isPrime = true;
+// let user = 7;
+// let isPrime = true;
 
 
-for (i = 2; i < user; i++) {
-    if (user % i == 0) {
-        isPrime = false;
-        break;
-    } 
-}
-if (isPrime== true) {
-    console.log(user, "is prime number")
-} else {
-    console.log(user, "is not prime number")
+// for (i = 2; i < user; i++) {
+//     if (user % i == 0) {
+//         isPrime = false;
+//         break;
+//     } 
+// }
+// if (isPrime== true) {
+//     console.log(user, "is prime number")
+// } else {
+//     console.log(user, "is not prime number")
+// }
+
+let userInput = ""
+var showData = document.getElementById('showData')
+document.getElementById("search").onkeyup = function () {
+    userInput = this.value;
+    
+    
+    
+    
+    let isPrimeNumber = true;
+
+    if (userInput == 1) {
+        showData.innerHTML = `${userInput}= is prime and composite number..`
+    }
+    else {
+        for (i = 2; i < userInput; i++) {
+            if (userInput % i == 0) {
+                isPrimeNumber = false;
+            }
+        }
+        if (isPrimeNumber) {
+            showData.innerHTML = `${userInput}= is Prime number`
+        } else {
+            showData.innerHTML = `${userInput}= is not Prime number`
+
+        }
+    }
 }
