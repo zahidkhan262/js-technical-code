c340ebe21f21dd13a30dd0050756e9553935bed1
zahidkhan262 <zahidkhan007860@gmail.com>
2022-01-25 23:40:27 +0530

    convert int to str

--- day01.js
+++ day01.js
@@ -12,3 +12,22 @@ let intToString = 12;
 console.log(typeof (intToString + ""))
 
 
+// (function (a) {
+//     return (function () {
+//         console.log(a);
+//         a = 6;
+//     })()
+// })(21);
+
+
+
+// What will be the output of the following code snippet?
+let abc=function solve(arr, rotations) {
+    if (rotations == 0) return arr;
+    for (let i = 0; i < rotations; i++) {
+        let element = arr.pop();
+        arr.unshift(element);
+    }
+    return arr;
+}
+console.log(abc)
