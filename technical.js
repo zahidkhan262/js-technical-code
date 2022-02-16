d46fda96350499ea2b658680f7de87ef1fa91fd3
zahidkhan262 <zahidkhan007860@gmail.com>
2022-02-16 12:29:19 +0530

    closets number

---
+++ technical.js
@@ -0,0 +1,19 @@
+let arr1 = [1, 2, 3, 4, 5, 6, 7];
+let arr2 = [5, 6, 7, 8, 9, 10, 11];
+
+let findInterSection = arr1.filter((ele) => {
+    return arr2.includes(ele)
+})
+let a = -12.411111
+console.log(Math.abs(a));
+console.log("findInterSection", findInterSection)
+
+const closets = 8;
+const numbers = [1, 10, 7, 2, 4, 9];
+
+let closeNumber = numbers.sort((a, b) => {
+    return Math.abs(closets - a) - Math.abs(closets - b);
+})
+
+console.log(closeNumber.shift());
+
