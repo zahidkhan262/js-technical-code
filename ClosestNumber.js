d779df30be1830073e36651932df5db5d023eb0e
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-02-25 22:31:42 +0530

    Update ClosestNumber.js

--- ClosestNumber.js
+++ ClosestNumber.js
@@ -1,8 +1,9 @@
-let arr = [11, 2, 3, 4, 67, 44, 10]
+let arr1 = [11, 2, 3, 4, 67, 44, 10]
 
 let target = 5;
 
-let findClosest = arr.reduce((prev, curr) => {
-    return (Math.abs(curr - target) < Math.abs(prev - target) ? curr : prev)
+let findClosest = arr1.reduce((prevElement, currEle) => {
+    return (Math.abs(currEle - target) < Math.abs(prevElement - target) ? currEle : prevElement)
 })
+
 console.log(findClosest);
