6ec2bd92af0e6eb3006cd4a0744bd43a0780a4b6
zahidkhan262 <zahidkhan007860@gmail.com>
2022-03-29 01:06:20 +0530

    async await++

--- asycnAwait.js
+++ asycnAwait.js
@@ -3,3 +3,23 @@ let data = [
     { name: "Abuzar", title: "developer" },
     { name: "yosuf", title: "developer" },
 ];
+function getData() {
+    setTimeout(() => {
+        let showData = "";
+        data.map((ele, index) => {
+            showData += `${ele.title} </br>`
+        })
+        console.log(showData);
+        document.getElementById("bind").innerHTML = showData;
+    }, 1000);
+}
+function addData(newData) {
+    setTimeout(() => {
+        data.push(newData)
+    }, 2000);
+}
+addData({ name: "Abuzar", title: "Tester" })
+getData()
+
+// if above funtion getData time is less than addData then only output is getData...
+// so by using asycn await we can resolve it
