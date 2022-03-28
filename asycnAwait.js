c4cf8f2ac714271143a0c52535905d71138c1578
zahidkhan262 <zahidkhan007860@gmail.com>
2022-03-29 01:24:15 +0530

    async await++

--- asycnAwait.js
+++ asycnAwait.js
@@ -13,13 +13,69 @@ function getData() {
         document.getElementById("bind").innerHTML = showData;
     }, 1000);
 }
-function addData(newData) {
-    setTimeout(() => {
-        data.push(newData)
-    }, 2000);
-}
-addData({ name: "Abuzar", title: "Tester" })
-getData()
+
+
+// function addData(newData) {
+//     setTimeout(() => {
+//         data.push(newData)
+//     }, 2000);
+// }
+// addData({ name: "Abuzar", title: "Tester" })
+// getData()
+
+// //  output=> developer
+// developer
+// developer
 
 // if above funtion getData time is less than addData then only output is getData...
-// so by using asycn await we can resolve it
+
+
+// so by using call back funtion we can resolve it
+
+// function addData(newData, callback) {
+//     setTimeout(() => {
+//         data.push(newData);
+//         callback()
+//     }, 2000);
+// }
+// addData({ name: "Abuzar", title: "Tester" }, getData)
+// //  output=> developer
+// developer
+// developer
+// Tester
+
+// by using Promise we can resolve it
+
+function addData(newData) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.push(newData);
+
+            let errorMessage = false;
+            if (!errorMessage) {
+                resolve()
+            } else {
+                reject("error ddata not found")
+            }
+
+        }, 2000);
+    })
+}
+// addData({ name: "Abuzar", title: "Tester" })
+//     .then(getData)
+//     .catch((error) => {
+//         console.log(error)
+//     })
+
+// developer
+// developer
+// developer
+// Tester
+
+// by using async await function 
+async function start() {
+    await addData({ name: "Abuzar", title: "Tester" })
+    getData()
+}
+start()
+
