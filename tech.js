f24de3e6506c6466b1704509ecb010622d07d96d
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-04-08 19:47:28 +0530

    Update tech.js

--- tech.js
+++ tech.js
@@ -13,6 +13,41 @@
 
 
 
+function xml2json(srcDOM) {
+  let children = [...srcDOM.children];
+
+  // base case for recursion.
+  if (!children.length) {
+    return srcDOM.innerHTML;
+  }
+
+  // initializing object to be returned.
+  let jsonResult = {};
+
+  for (let child of children) {
+    // checking is child has siblings of same name.
+    let childIsArray =
+      children.filter((eachChild) => eachChild.nodeName === child.nodeName)
+        .length > 1;
+
+    // if child is array, save the values as array, 
+    // else as strings.
+    if (childIsArray) {
+      if (jsonResult[child.nodeName] === undefined) {
+        jsonResult[child.nodeName] = [xml2json(child)];
+      } else {
+        jsonResult[child.nodeName].push(xml2json(child));
+      }
+    } else {
+      jsonResult[child.nodeName] = xml2json(child);
+    }
+  }
+
+  return jsonResult;
+}
+
+
+
 // for (h in myObj.cars) {
 //     document.write(myObj.cars[h].name, "<br>")
 //     myObj.cars[h].models.forEach((g) => {
