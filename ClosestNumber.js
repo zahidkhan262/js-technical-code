aad190ae4ccf9f59cf3d8468269138bf6caa1760
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-04-14 21:25:42 +0530

    random number b/w 2 numer

--- ClosestNumber.js
+++ ClosestNumber.js
@@ -20,5 +20,10 @@ var c = 0.3;
 var d=a+b-c ==val;
     document.body.innerHTML=d
 
+function randomFunc(ran1, ran2){
+    let findRandomNumber = Math.floor(Math.random() * (ran1 -ran2 +1))
+    return findRandomNumber;
+}
+randomFunc(1,20)
 
 // https://netsuitedocumentation1.gitlab.io/netsuitedocumentation1/SCADeveloper.pdf
