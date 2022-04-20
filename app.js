fd5a50e768ccfcff69d8e677a5ebe20a520e4ac2
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2022-04-20 19:26:40 +0530

    hello world

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-function searchClick() {
-    let searchClass = document.getElementsByClassName('input_field');
-    searchClass[0].classList.toggle('active')
+const express= require('express');
+const app= express();
+
+const PORT=4000;
+const customeMiddleWare =(res, req,next)=>{
+    console.log("middleware")
+    next()
 }
+
+app.use(customeMiddleWare)
+
+app.get('/',(res, req)=>{
+    console.log("zahid khan");
+    res.send('hello world");
+});
+// create middle for about so inclide as second parameter in app.get
+app.get('/about',customeMiddleWare,(res, req)=>{
+    console.log("zahid khan");
+    res.send('My name is Zahid khan I am frontend React js developer");
+})
+    
+app.listen(PORT,()=>{
+    console.log(`server is live on this port ${PORT}`)
+})
+    
