c8b48fcc3f6f8f07e72171ac4b079d0764fbd5b1
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2023-01-20 11:26:44 +0530

    count down++

--- technical.js
+++ technical.js
@@ -24,3 +24,21 @@ let closeNumber = numbers.sort((a, b) => {
 
 console.log(closeNumber.shift());
 
+
+// recursion countown
+// program to count down numbers to 1
+function countDown(number) {
+
+    // display the number
+    console.log(number);
+
+    // decrease the number value
+    const newNumber = number - 1;
+
+    // base case
+    if (newNumber > 0) {
+        countDown(newNumber);
+    }
+}
+
+countDown(4);
