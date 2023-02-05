3f8481567f600ce4d58f9b7a9eb529c1c9539321
zahidkhan <zahidkhan007860@mail.com>
2023-02-05 22:45:16 +0530

    remove comment

--- array/duplicateValue.js
+++ array/duplicateValue.js
@@ -1,7 +1,6 @@
 let data = [20, 20, 40, 2, 30, 50, 2, 30, 55, 50, 1, 6, 9, 9]
 
 const findDuplicateValue = (arr) => {
-    debugger
     let duplicate = [];
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j <= i; j++) {
