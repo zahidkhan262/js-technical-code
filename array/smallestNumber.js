718f7aaec587594451d61d2f47d804a9c7e13df0
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2023-02-12 21:31:12 +0530

    smallest number

--- array/smallestNumber.js
+++ array/smallestNumber.js
@@ -3,12 +3,12 @@ let data = [20, 40, 2, 30, 50, 60]
 const findSmallestNumber = (arr) => {
     // let small = 1
     // ?first way
-    // let small = arr.sort((a, b) => a - b).shift()
-    // return small
+    let small = arr.sort((a, b) => a - b).shift()
+    return small
 
     // ?second way
-    // let min = Math.min(...arr)
-    // return min
+    let min = Math.min(...arr)
+    return min
 
 }
 console.log(findSmallestNumber(data), "smallest number")
